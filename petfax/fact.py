24d295c6fcefd3cb3905ec1a527122ae357268a1
Manuel Estrada <manuel.estrada16@gmail.com>
2023-01-25 19:12:08 -0500

    activity ppp-6-requests-and-redirects

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,16 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request ) 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
 @bp.route('/new_fact')
 def new(): 
-    return render_template('facts/new_fact.html')
+    return render_template('facts/new.html')
+
+
+@bp.route('/', methods=['GET', 'POST']) 
+def index(): 
+    if request.method == 'POST':
+       print(request.form)
+       return redirect('/facts')
+    
+    return render_template('facts/index.html')
